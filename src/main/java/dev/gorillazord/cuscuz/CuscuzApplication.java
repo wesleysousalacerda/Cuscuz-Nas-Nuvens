11d95617671fa64c8555ed4a6604ef8b17ce200a
Hilton Wesley de Sousa Lacerda <wesleysousalacerda@gmail.com>
2024-07-03 09:42:51 -0300

    Cuscuz Application Started
    An application for Cuscuz delivery and Spring practices

---
+++ src/main/java/dev/gorillazord/cuscuz/CuscuzApplication.java
@@ -0,0 +1,13 @@
+package dev.gorillazord.cuscuz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuscuzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuscuzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/gorillazord/cuscuz/CuscuzApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gorillazord.cuscuz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuscuzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
