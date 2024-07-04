49af89f1be1ece12ef4764b94692d1c974edc407
Hilton Wesley de Sousa Lacerda <wesleysousalacerda@gmail.com>
2024-07-04 11:00:23 -0300

    Configuration file to handle routing

--- src/test/java/dev/gorillazord/cuscuz/controller/HomeControllerTest.java
+++ src/test/java/dev/gorillazord/cuscuz/controller/HomeControllerTest.java
@@ -10,12 +10,11 @@ import static
 org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static
 org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(HomeController.class)
 public class HomeControllerTest {
 
     @Autowired
