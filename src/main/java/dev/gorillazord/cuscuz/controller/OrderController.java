a69c4b48580b32b96d8de31bc683790df064ea34
Hilton Wesley de Sousa Lacerda <wesleysousalacerda@gmail.com>
2024-07-04 09:23:18 -0300

    fixed mapping error and created styleseet

--- src/main/java/dev/gorillazord/cuscuz/controller/OrderController.java
+++ src/main/java/dev/gorillazord/cuscuz/controller/OrderController.java
@@ -2,8 +2,10 @@ package dev.gorillazord.cuscuz.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import lombok.extern.slf4j.Slf4j;
 import dev.gorillazord.cuscuz.model.CuscuzOrder;
@@ -17,5 +19,15 @@ public class OrderController {
     public String orderForm() {
         return "orderForm";
     }
+
+@PostMapping
+public String processOrder(CuscuzOrder order,
+ SessionStatus sessionStatus) {
+ log.info("Order submitted: {}", order);
+ sessionStatus.setComplete();
+ return "redirect:/";
+}
+
+
 
 }
